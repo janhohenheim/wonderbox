27194eaa196d22a5750fcc68e116af829b976cca
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-01 21:26:54 +0200

    Add lints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 #![feature(specialization)]
+#![warn(missing_docs, clippy::dbg_macro, clippy::unimplemented)]
+#![deny(
+    rust_2018_idioms,
+    future_incompatible,
+    missing_debug_implementations,
+    clippy::doc_markdown,
+    clippy::default_trait_access,
+    clippy::enum_glob_use,
+    clippy::needless_borrow,
+    clippy::large_digit_groups,
+    clippy::explicit_into_iter_loop
+)]
 
 use core::any::TypeId;
 use std::any::Any;
