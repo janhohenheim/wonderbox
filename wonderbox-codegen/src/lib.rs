895f274913a37fa8ccd7fc350cdd1f91ef6cab7f
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-03 08:15:30 +0200

    Improve debug printing

--- wonderbox-codegen/src/lib.rs
+++ wonderbox-codegen/src/lib.rs
@@ -7,6 +7,6 @@ use syn;
 #[proc_macro_attribute]
 pub fn constructor(attr: TokenStream, item: TokenStream) -> TokenStream {
     println!("attr: \"{}\"", attr.to_string());
-    println!("item: \"{}\"", item.to_string());
+    println!("item: \"{:#?}\"", item.to_string());
     item
 }
