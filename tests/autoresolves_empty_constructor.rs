91db34ea6a0e01ef8f1c25f048a109cc6f8deab2
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-05 15:06:17 +0200

    Add integration test for empty ctor

---
+++ tests/autoresolves_empty_constructor.rs
@@ -0,0 +1,18 @@
+use wonderbox::Container;
+use wonderbox_codegen::resolve_dependencies;
+
+#[derive(Debug, Default)]
+struct Foo;
+
+#[resolve_dependencies]
+impl Foo {
+    fn new() -> Self {
+        Foo::default()
+    }
+}
+
+#[test]
+fn test() {
+    let mut container = Container::new();
+    container.register_autoresolved(|foo: Option<Foo>| foo.unwrap());
+}
