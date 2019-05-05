93c59b2a81478aa6b1816e97613453a803e4bb1e
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-05 16:12:19 +0200

    Add autoresolution test for trait objects

---
+++ tests/autoresolved_empty_ctor_can_be_resolved_as_trait_object.rs
@@ -0,0 +1,25 @@
+use wonderbox::Container;
+use wonderbox_codegen::resolve_dependencies;
+
+trait Foo {}
+
+#[derive(Debug, Default)]
+struct FooImpl;
+
+#[resolve_dependencies]
+impl FooImpl {
+    fn new() -> Self {
+        Foo::default()
+    }
+}
+
+impl Foo for FooImpl {}
+
+#[test]
+fn test() {
+    let mut container = Container::new();
+    container.register_autoresolved(|foo: Option<Foo>| Box::new(foo.unwrap()) as Box<dyn Foo>);
+
+    let foo = container.resolve::<Box<dyn Foo>>();
+    assert!(foo.is_some());
+}
