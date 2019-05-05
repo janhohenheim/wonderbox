95c9fcb117531bcc9e48e1aef59254b96ccbc463
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-05 16:18:34 +0200

    Fix types in autoresolution of trait object

--- tests/autoresolved_empty_ctor_can_be_resolved_as_trait_object.rs
+++ tests/autoresolved_empty_ctor_can_be_resolved_as_trait_object.rs
@@ -9,7 +9,7 @@ struct FooImpl;
 #[resolve_dependencies]
 impl FooImpl {
     fn new() -> Self {
-        Foo::default()
+        FooImpl::default()
     }
 }
 
@@ -18,7 +18,7 @@ impl Foo for FooImpl {}
 #[test]
 fn test() {
     let mut container = Container::new();
-    container.register_autoresolved(|foo: Option<Foo>| Box::new(foo.unwrap()) as Box<dyn Foo>);
+    container.register_autoresolved(|foo: Option<FooImpl>| Box::new(foo.unwrap()) as Box<dyn Foo>);
 
     let foo = container.resolve::<Box<dyn Foo>>();
     assert!(foo.is_some());
