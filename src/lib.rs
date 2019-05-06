cb06247086989c8ac6eed1472c3c9da5e999c460
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-06 09:18:48 +0200

    Fix doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -128,28 +128,30 @@ impl Container {
     /// # Examples
     /// ```
     /// use wonderbox::Container;
+    /// use wonderbox_codegen::resolve_dependencies;
+    ///
     /// trait Foo {}
     ///
     /// #[derive(Debug, Default)]
     /// struct FooImpl {
-    /// stored_string: String,
+    ///     stored_string: String,
     /// }
     ///
     /// #[resolve_dependencies]
     /// impl FooImpl {
-    /// fn new(stored_string: String) -> Self {
-    /// Self { stored_string }
-    /// }
+    ///     fn new(stored_string: String) -> Self {
+    ///         Self { stored_string }
+    ///     }
     /// }
     ///
     /// impl Foo for FooImpl {}
     ///
-    ///     let mut container = Container::new();
-    ///     container.register_clone("foo".to_string());
-    ///     container.register_autoresolved(|foo: Option<FooImpl>| Box::new(foo.unwrap()) as Box<dyn Foo>);
+    /// let mut container = Container::new();
+    /// container.register_clone("foo".to_string());
+    /// container.register_autoresolved(|foo: Option<FooImpl>| Box::new(foo.unwrap()) as Box<dyn Foo>);
     ///
-    ///     let foo = container.resolve::<Box<dyn Foo>>();
-    ///     assert!(foo.is_some())
+    /// let foo = container.resolve::<Box<dyn Foo>>();
+    /// assert!(foo.is_some())
     /// ```
     pub fn register_autoresolved<ResolvedType, RegisteredType>(
         &mut self,
