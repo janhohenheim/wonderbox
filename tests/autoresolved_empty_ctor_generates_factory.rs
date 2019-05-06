50d998f3d9fd732bd726741ed13a82b64acef69a
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-06 09:43:59 +0200

    Add factory tests

---
+++ tests/autoresolved_empty_ctor_generates_factory.rs
@@ -0,0 +1,21 @@
+use wonderbox::Container;
+use wonderbox_codegen::resolve_dependencies;
+
+#[derive(Debug, Default)]
+struct Foo;
+
+#[resolve_dependencies]
+impl Foo {
+    fn new() -> Self {
+        Foo::default()
+    }
+}
+
+#[test]
+fn test() {
+    let mut container = Container::new();
+    container.register_autoresolved(|foo: Option<Foo>| foo.unwrap());
+
+    let foo_factory = container.resolve::<Box<dyn Fn() -> Foo>>();
+    assert!(foo_factory.is_some());
+}
