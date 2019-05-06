12cf01b17751f5c8b4e200ed6c01dc723d176ed0
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-06 13:48:35 +0200

    Add module example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,37 @@
 //! A minimalistic [IoC] library.
 //!
+//! # Examples
+//! ```
+//! use wonderbox::{register, Container};
+//! use wonderbox_codegen::resolve_dependencies;
+//!
+//! trait Foo {}
+//!
+//! #[derive(Debug, Default)]
+//! struct FooImpl {
+//!     stored_string: String,
+//! }
+//!
+//! #[resolve_dependencies]
+//! impl FooImpl {
+//!     fn new(stored_string: String) -> Self {
+//!         Self { stored_string }
+//!     }
+//! }
+//!
+//! impl Foo for FooImpl {}
+//!
+//! #[test]
+//! fn test() {
+//!     let mut container = Container::new();
+//!     container.register_clone("foo".to_string());
+//!     register!(container, FooImpl as Box<dyn Foo>);
+//!
+//!     let foo = container.resolve::<Box<dyn Foo>>();
+//!     assert!(foo.is_some())
+//! }
+//! ```
+//!
 //! [IoC]: https://en.wikipedia.org/wiki/Inversion_of_control
 
 #![feature(custom_attribute)]
